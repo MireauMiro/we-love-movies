b4258f0d85b8abc4338673db9e2332bbd5aeb7fd
MireauMiro <geoffrey.karr@gmail.com>
2023-08-29 14:04:39 -0700

    adding CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,7 @@ const moviesRouter = require("./movies/movies.router");
 const criticsRouter = require("./critics/critics.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
+app.use(cors());
 app.use("/theaters", theatersRouter);
 app.use("/movies", moviesRouter);
 app.use("/critics", criticsRouter);
